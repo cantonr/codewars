d8ba6734c8f330bdd213f8c54ed6b7fef0ad8c02
Ryan Canton <ryan.canton@cleverbridge.com>
2018-01-07 17:19:38 -0600

    finish tenPinBowling kata

--- tenPinBowling/tenPinBowling.js
+++ tenPinBowling/tenPinBowling.js
@@ -3,59 +3,123 @@ function bowlingScore(frames) {
   var frameScores = [];
   var totalScore = 0;
 
-  console.log('scoreArr: ', scoreArr);
-
   scoreArr.forEach(function(val, index) {
     var splitVal = val.split('').map( (val) => isNaN(parseInt(val)) ? val : parseInt(val));
     if (index != 9) {
       if (val === 'X') {
-        if (index === 0) {
+        if (index === 0) { // first frame
           frameScores[index] = 10;
-        }
-        if (index === 1) {
+        } else if (index === 1) { // second frame
           frameScores[index] = 10;
-          if (scoreArr[index-1] == 'X') {
+          if (scoreArr[index-1] == 'X' || scoreArr[index-1].match('/') != null) {
             frameScores[index-1] += 10
-          } else {
-
+          }
+        } else { // 3rd or greater frame
+          frameScores[index] = 10;
+          if (scoreArr[index-1] == 'X' && scoreArr[index-2] == 'X') {
+            frameScores[index-1] += 10
+            frameScores[index-2] += 10
+          } else if (scoreArr[index-1] == 'X' || scoreArr[index-1].match('/') != null) {
+            frameScores[index-1] += 10
           }
         }
-        if (index >= 2) {
-          frameScores[index] = 10;
-          frameScores[index-1] == 10 ? frameScores[index-1] += 10 : false
-          frameScores[index-2] == 20 ? frameScores[index-2] += 10 : false
-        }
       } else if (splitVal[1] === '/') {
-        frameScores[index] = 10;
+        if (index === 0) { // first frame
+          frameScores[index] = 10;
+        } else if (index === 1) { // second frame
+          frameScores[index] = 10;
+          if (scoreArr[index-1] == 'X') {
+            frameScores[index-1] += 10;
+          } else if (scoreArr[index-1].match('/') != null) {
+            frameScores[index-1] += splitVal[0];
+          }
+        } else { // 3rd or greater frame
+          frameScores[index] = 10;
+          if (scoreArr[index-1] == 'X' && scoreArr[index-2] == 'X') {
+            frameScores[index-1] += 10;
+            frameScores[index-2] += splitVal[0];
+          } else if (scoreArr[index-1].match('/') != null) {
+            frameScores[index-1] += splitVal[0];
+          } else if (scoreArr[index-1] == 'X') {
+            frameScores[index-1] += 10;
+          }
+        }
       } else {
         if (index === 0) {
           frameScores[index] = splitVal[0] + splitVal[1];
-        }
-        if (index === 1) {
+        } else if (index === 1) {
           frameScores[index] = splitVal[0] + splitVal[1];
-          scoreArr[index-1] !== 'X' ? frameScores[index-1] += splitVal[0] : false
-        }
-        if (index >= 2) {
+          if (scoreArr[index-1] == 'X') {
+            frameScores[index-1] += splitVal[0] + splitVal[1];
+          } else if (scoreArr[index-1].match('/') != null) {
+            frameScores[index-1] += splitVal[0];
+          }
+        } else {
           frameScores[index] = splitVal[0] + splitVal[1];
-          frameScores[index-1] == 10 ? frameScores[index-1] += 10 : false
-          frameScores[index-2] == 20 ? frameScores[index-2] += 10 : false
+          if (scoreArr[index-1] == 'X' && scoreArr[index-2] == 'X') {
+            frameScores[index-1] += splitVal[0];
+            frameScores[index-2] += splitVal[0] + splitVal[1];
+          } else if (scoreArr[index-1] == 'X') {
+            frameScores[index-1] += splitVal[0] + splitVal[1];
+          } else if (scoreArr[index-1].match('/') != null) {
+            frameScores[index-1] += splitVal[0];
+          }
         }
       }
-    } else {
-
-    }
-    console.log('frameScores', index,': ', frameScores[index]);
-    // console.log('index: ', index);
-    // var frame = val.split('');
+    } else { // handle 10th frame
+      if (splitVal.length === 2) {
+        frameScores[index] = splitVal[0] + splitVal[1];
+        if (scoreArr[index-1] == 'X' && scoreArr[index-2] == 'X') {
+          frameScores[index-1] += splitVal[0];
+          frameScores[index-2] += splitVal[0] + splitVal[1];
+        } else if (scoreArr[index-1] == 'X') {
+          frameScores[index-1] += splitVal[0] + splitVal[1];
+        } else if (scoreArr[index-1].match('/') != null) {
+          frameScores[index-1] += splitVal[0];
+        }
+      } else {
+        if (splitVal[0] == 'X') {
+          frameScores[index] = 10;
+          if (scoreArr[index-1] == 'X' && scoreArr[index-2] == 'X') {
+            frameScores[index-1] += 10
+            frameScores[index-2] += 10
+          } else if (scoreArr[index-1] == 'X' || scoreArr[index-1].match('/') != null) {
+            frameScores[index-1] += 10
+          }
+          if (splitVal[1] == 'X') {
+            frameScores[index] += 10;
+            if (scoreArr[index-1] == 'X') {
+              frameScores[index-1] += 10;
+            }
+            if (splitVal[2] == 'X') {
+              frameScores[index] += 10;
+            } else {
+              frameScores[index] += splitVal[2];
+            }
+          } else if (splitVal[2] == '/') {
+            frameScores[index] += 10;
+            if (scoreArr[index-1] == 'X') {
+              frameScores[index-1] += splitVal[1];
+            }
+          }
+        } else {
+          splitVal[2] == 'X' ? frameScores[index] = 20 : frameScores[index] = 10 + splitVal[2];
+          if (scoreArr[index-1] == 'X' && scoreArr[index-2] == 'X') {
+            frameScores[index-1] += splitVal[0];
+            frameScores[index-2] += splitVal[0];
+          } else if (scoreArr[index-1].match('/') != null) {
+            frameScores[index-1] += splitVal[0];
+          } else if (scoreArr[index-1] == 'X') {
+            frameScores[index-1] += 10;
+          }
+        }
+      } // end else (splitVal > 2)
+    } // end else (handle 10th)
   }); // end forEach
 
-  console.log(frameScores)
   frameScores.map(function(val) {
     totalScore += val;
   });
-  console.log('Total Score: ', totalScore)
+
   return totalScore;
 }
-
-// bowlingScore('X X 9/ 80 X X X 8/ 7/ 44');
-bowlingScore('X X X X X X X X X 9/9');
--- tenPinBowling/userSubmissions.md
+++ tenPinBowling/userSubmissions.md
@@ -1,4 +1,78 @@
-[]()
+[MMMAAANNN](https://www.codewars.com/users/MMMAAANNN)
 ```js
+function bowlingScore(frames) {
+  var rolls = frames.replace(/ /g, '').split(''),
+      count = 0,
+      score = 0,
+      frame;
+  for (var i = 0; i < rolls.length; i++) {
+    if (rolls[i] === 'X') rolls[i] = 10;
+    if (rolls[i] === '/') rolls[i] = 10 - rolls[i - 1];
+    else rolls[i] = parseInt(rolls[i]);
+  }
+  while (count < 10) {
+    count++;
+    frame = rolls.shift();
+    if (frame === 10) score += frame + rolls[0] + rolls[1];        // Strike!!
+    else {
+      frame += rolls.shift();
+      score += frame;
+      if (frame === 10) score += rolls[0];                         // Spare!
+    }
+  }
+  return score;
+}
+```
+[jegan145](https://www.codewars.com/users/jegan145)
+```js
+function bowlingScore(framesString) {
+  const balls = framesString.replace(/ /g, '').split('')
 
+  let totalScore = 0
+  for (let i = 0, frame = 0; i < balls.length && frame < 10; frame++ ) {    
+    totalScore += _frameScore(balls[i], balls[i + 1], balls[i + 2])
+    if (balls[i] === 'X') i += 1
+    else i += 2
+  }
+
+  return totalScore
+}
+
+function _frameScore(ball1, ball2, ball3) {
+  if (ball1 === 'X') return 10 + _twoBallScore(ball2, ball3)
+  if (ball2 === '/') return 10 + _nextBallScore(ball3)
+  return _twoBallScore(ball1, ball2)
+}
+
+function _twoBallScore(ball1, ball2) {
+  return ball2 === '/' ? 10 : _nextBallScore(ball1) + _nextBallScore(ball2)
+}
+
+function _nextBallScore(ball) {
+  return ball === 'X' ? 10 : parseInt(ball)
+}
+```
+[vespaiach](https://www.codewars.com/users/vespaiach)
+```js
+function bowlingScore(str) {
+  const rolls = str.replace(/\s/g, '').split('');
+  const scores = rolls.map((roll, i) => roll === 'X' ? 10 : roll === '/' ? 10 - parseInt(rolls[i - 1]) : parseInt(roll));
+  const max = rolls.length - str.substring(str.lastIndexOf(' ') + 1).length;
+
+  return rolls.reduce((total, val, i) => {
+    if (i < max) {
+      if (val === 'X') {
+        total += scores[i] + scores[i + 1] + scores[i + 2];
+      } else if (val === '/') {
+        total += scores[i] + scores[i + 1];
+      } else {
+        total += scores[i];
+      }
+    } else {
+      total += scores[i];
+    }
+
+    return total;
+  }, 0);
+}
 ```
